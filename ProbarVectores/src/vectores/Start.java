6e3895cc3911296f40a6a35dff1af133c3cf2f09
Mañana <Maana@DESKTOP-KI0CM61>
2023-01-24 12:45:37 +0100

    Versión borrarNumero

---
+++ ProbarVectores/src/vectores/Start.java
@@ -0,0 +1,32 @@
+package vectores;
+
+public class Start {
+
+		public static void main(String[] args) {
+
+			Vectorx v = new Vectorx();
+			v.add(2);
+			v.add(100);
+			v.add(7);
+			v.add(8);
+			v.add(100);
+			v.add(20);
+			v.add(100);
+			v.add(100);
+			v.add(650);
+			
+			
+			int donde = v.buscar(100, 0);
+			while(donde!=-1) {
+				
+				System.out.println(donde);
+				donde = v.buscar(100, donde+1);
+			}
+			
+			System.out.println(v);
+			v.borrarNumero(2);
+			System.out.println(v);
+			
+		}
+
+}
---
+++ ProbarVectores/src/vectores/Vectorx.java
@@ -0,0 +1,171 @@
+package vectores;
+
+import java.util.Arrays;
+
+public class Vectorx {
+
+	private int[] datos;
+	private int elementos;
+	
+	public Vectorx() {
+		
+		datos = new int[10];
+		elementos = 0;
+	}
+
+	
+	
+	/**
+	 * rellena aleatoriamente el vector de vectorx
+	 */
+	public void aleatorio() {
+		
+		elementos = datos.length;
+		for(int i=0;i<datos.length;i++) {
+			
+			datos[i] = (int)(Math.random()*100);
+		}
+		
+	}
+	
+	
+	
+	
+	
+	public MaxMin getMaxMin() {
+		
+		MaxMin devolver = new MaxMin();
+
+		devolver.setMax(getMaximo());
+		devolver.setMin(getMinimo());
+		
+		return devolver;
+	}
+	
+	
+	
+	
+	
+	private int getMinimo() {
+		
+		int minimo = 10000;
+		
+		for(int i=0;i<elementos;i++) {
+			
+			if(minimo>datos[i]) {
+				minimo = datos[i];
+			}
+		}
+		return minimo;
+		
+	}	
+	
+	public int[] borrarNumero(int numero) {
+		
+		boolean borrado = false;
+		int longitudNueva = 0;
+		
+		for(int i=0; i<datos.length; i++) {
+			
+			if((!borrado)) {
+				
+				if(datos[i]!=numero) {
+					datos[i]=datos[i];
+				}
+				else if(datos[i]==numero) {
+					datos[i]=datos[i+1];
+					borrado = true;
+				}
+			}
+			else if(borrado) {
+				longitudNueva = datos.length-1;
+				if(i==longitudNueva) {
+					i=datos.length;
+				}
+				else{
+					datos[i]=datos[i+1];
+				}
+			}
+		}
+		return datos;
+	}
+	
+	
+	private int getMaximo() {
+		
+		int maximo = -10000;
+		
+		for(int i=0;i<elementos;i++) {
+			
+			if(maximo<datos[i]) {
+				maximo = datos[i];
+			}
+		}
+		return maximo;
+	}
+	
+	
+	
+	
+	/**
+	 * añadimos un número al vector que hay en vectorx
+	 * @param numero
+	 */
+	public void add(int numero) {
+		
+		datos[elementos] = numero;
+		elementos++;
+		
+		if(elementos==datos.length) {
+			elementos = 0;
+		}
+	}
+	
+	
+	
+	public int buscar(int numero,int donde) {
+		
+		int i = donde;
+		boolean encontrado = false;
+		while(i<datos.length&&(!encontrado)) {
+			
+			if(numero==datos[i]) {
+				
+				encontrado = true;
+			}
+			else i++;
+		}
+		if(encontrado) return i; else return -1;
+	}
+	
+	
+	
+	
+	 
+	
+	/**
+	 * nosotrtos controlmos el toString, decidiendo qué
+	 * sale en ese String
+	 */
+	@Override
+	public String toString() {
+		
+		String devolver = "[";
+		
+		for(int i=0;i<datos.length;i++) {
+			
+			devolver += datos[i]+",";
+		}
+		//devolver += datos[elementos-1];
+		devolver+="]";
+		
+		return devolver;
+		
+	}
+		
+		
+		
+
+	}
+
+
